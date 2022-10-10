1447e4f54f2a1a28939da2284fb167869e58415f
prionkar-barua <prionkar3301@gmail.com>
2022-10-10 23:29:46 +0600

    Codeforces problem

--- 1360B_Honest_Coach.cpp
+++ 1360B_Honest_Coach.cpp
@@ -7,7 +7,7 @@ int main(){
     while(t--){
         int n,min=INT_MAX;cin>>n;
         int arr[n];
-	    for(int i=0;i<n;i++) cin>>arr[i];
+        for(int i=0;i<n;i++) cin>>arr[i];
         sort(arr,arr+n);
         for(int i=0;i<n-1;i++){
             if((arr[i+1]-arr[i])<min) min=arr[i+1]-arr[i];
